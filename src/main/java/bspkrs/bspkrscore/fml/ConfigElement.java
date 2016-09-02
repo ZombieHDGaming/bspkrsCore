4cd667641d8227e69e11b733244223e7c230ac7f
jco2641 <john.oconnell@gmail.com>
2016-09-01 20:59:08 -0700

    Remove last trace of bspkrs update code in config file

--- src/main/java/bspkrs/bspkrscore/fml/ConfigElement.java
+++ src/main/java/bspkrs/bspkrscore/fml/ConfigElement.java
@@ -7,16 +7,13 @@ import static net.minecraftforge.common.config.Property.Type.INTEGER;
 
 public enum ConfigElement
 {
-    ALLOW_UPDATE_CHECK("allowUpdateCheck", "bspkrs.configgui.allowUpdateCheck", "Set to true to allow checking for updates for ALL of my mods, false to disable.", BOOLEAN),
     ALLOW_DEBUG_OUTPUT("allowDebugOutput", "bspkrs.configgui.allowDebugOutput", "", BOOLEAN),
     UPDATE_TIMEOUT_MILLISECONDS("updateTimeoutMilliseconds", "bspkrs.configgui.updateTimeoutMilliseconds",
             "The timeout in milliseconds for the version update check.", INTEGER),
     GENERATE_UNIQUE_NAMES_FILE("generateUniqueNamesFile", "bspkrs.configgui.generateUniqueNamesFile",
             "When true a file called UniqueNames.txt will be generated in the config folder for convenience. " +
                     "The names found in the file are the string representation of blocks and items in Minecraft. " +
-                    "Mods such as Treecapitator and StartingInventory use them in their config files since IDs are gone.", BOOLEAN),
-    SHOW_MAIN_MENU_MOBS("showMainMenuMobs", "bspkrs.configgui.showMainMenuMobs",
-            "Set to true to show your logged-in player and a random mob on the main menu, false to disable.", BOOLEAN);
+                    "Mods such as Treecapitator and StartingInventory use them in their config files since IDs are gone.", BOOLEAN);
 
     private final String        key;
     private final String        langKey;
